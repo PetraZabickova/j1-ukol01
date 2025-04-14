2b2c6d51d19e93b9c23a99bb2189927fb4404d26
petrazabickova <petra.zabickova@mzk.cz>
2025-04-14 14:43:38 +0200

    Nakresli prasatko

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -3,16 +3,59 @@ package cz.czechitas.java.ukol01;
 import cz.czechitas.java.ukol01.engine.Turtle;
 
 public class HlavniProgram {
-  private Turtle zofka;
+    private Turtle zofka;
 
-  public static void main(String[] args) {
-    new HlavniProgram().start();
-  }
+    public static void main(String[] args) {
+        new HlavniProgram().start();
+    }
 
-  public void start() {
-    zofka = new Turtle();
+    public void start() {
+        zofka = new Turtle();
+        nakresliPrasatko();
+    }
 
-    //TODO implementace domácího úkolu
-  }
+    public void nakresliPrasatko() {
+        nakresliCtverec();
+
+        nakresliTrojuhelnik();
+
+        nakresliNozicky();
+    }
+
+    public void nakresliCtverec() {
+        for (int i = 0; i < 4; i++) {
+            zofka.move(200);
+            zofka.turnRight(90);
+        }
+    }
+
+    public void nakresliTrojuhelnik() {
+        for (int i = 0; i < 3; i++) {
+            zofka.move(200);
+            zofka.turnLeft(120);
+        }
+    }
+
+
+    public void nakresliNozicky() {
+
+        zofka.turnLeft(135);
+        zofka.move(100);
+        zofka.turnLeft(180);
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnLeft(180);
+        zofka.move(100);
+        zofka.turnRight(135);
+        zofka.move(200);
+        zofka.turnLeft(225);
+        zofka.move(100);
+        zofka.turnLeft(180);
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(100);
+    }
+
 
 }
